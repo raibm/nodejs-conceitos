f299cdb59e731e6b197124b6d6a7f0e545d95437
rai.maciel <rai.maciel@basis.com.br>
2020-06-15 15:36:38 -0300

    Nunjuck, exemplo basico

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
 const express = require('express');
+const nunjucks = require('nunjucks');
 
 const app = express();
 
-const logMiddleware = (req, res, next) => {
-    console.log(`HOST: ${req.headers.host} | URL: ${req.url} | METHOD: ${req.method}`);
-
-    req.appName = 'GoNode';
-
-    return next();
-}
-
-
-app.get("/", logMiddleware, (req, res) => {
-    return res.send(`Bem-vindo, ${req.query.name}, ${req.appName}`);
+nunjucks.configure('views', {
+   autoescape: true,
+   express: app,
+   watch: true 
 });
 
-app.get("/nome/:name", (req, res) => {
-    return res.json({
-        message: `Bem-vindo, ${req.params.name}`
-    });
+app.set('view engine', 'njk');
+
+app.get("/", (req, res) => {
+    return res.render('list', { name: "Rai" });
 });
 
 app.listen(3000);
